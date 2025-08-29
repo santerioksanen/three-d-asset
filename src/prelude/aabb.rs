ef8c1dfe339493c4ed9e36ec5e5c273bc56a273e
santeriok <santeri.oksanen@gmail.com>
2025-08-29 23:13:21 +0300

    Add intersects to aabb

--- src/prelude/aabb.rs
+++ src/prelude/aabb.rs
@@ -117,6 +117,20 @@ impl AxisAlignedBoundingBox {
         }
     }
 
+    /// Checks wether this aabb intersects with other 
+    pub fn intersects(&self, other: &Self) -> bool {
+        let min_a = self.min();
+        let max_a = self.max();
+        let min_b = other.min();
+        let max_b = other.max();
+
+        if min_a.x >= max_b.x || min_a.y >= max_b.y || min_b.x >= max_a.x || min_b.y >= max_a.y {
+            return false;
+        }
+
+        true
+    }
+
     /// Returns the intersection between this and the other given bounding box.
     pub fn intersection(self, other: Self) -> Self {
         let min_a = self.min();
